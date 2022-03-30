c664a160d7931f7649b14438fb71149025d8998b
Aleks Stancheva <aleksstancheva2005@gmail.com>
2022-03-30 22:18:35 +0300

    Code for display

--- main/main.ino
+++ main/main.ino
@@ -1,11 +1,55 @@
-#define SENSOR A0
-#define LED 9
+#include <LiquidCrystal.h> 
+
+//#define SENSOR A0
+//#define LED 9
+
+#define RS 11
+#define EN 12
+#define D4 2
+#define D5 3
+#define D6 4
+#define D7 5
+
+
+LiquidCrystal lcd(RS, EN, D4, D5, D6, D7);
 
 void setup() {
+  analogWrite(9, 600);
   Serial.begin(9600);
+
+  lcd.begin(16, 2);
+
+  byte customChar[8] = {
+    0b00000,
+    0b01010,
+    0b11111,
+    0b11111,
+    0b01110,
+    0b00100,
+    0b00000,
+    0b00000
+};
+
+  lcd.createChar(0, customChar);
+
+  lcd.setCursor(0, 0);
+  lcd.print("Hey hey!");
+
+  
+
+  lcd.setCursor(5, 0);
+  lcd.write((byte)0);
 }
 
 void loop() {
-  Serial.println(analogRead(SENSOR));
-  analogWrite(LED, 1024 - analogRead(SENSOR));
+  /*int sensor_value = analogRead(SENSOR);
+  Serial.println(sensor_value);*/
+  
+  /*if (sensor_value > 500) {
+    analogWrite(LED, sensor_value);
+  }
+  else {
+    analogWrite(LED, 0);
+  }*/
+
 }
